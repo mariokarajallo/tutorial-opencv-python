a2771632145f3b0fff653266a94df2bdb117e982
mariokarajallo <mario_karajallo@hotmail.com>
2022-05-26 02:20:28 -0400

    agregando la operacion binaria OR

--- 6-operaciones-binarias.py
+++ 6-operaciones-binarias.py
@@ -6,9 +6,14 @@ import numpy as np
 image1 = cv2.imread('image\square.jpg')
 image2 = cv2.imread('image\circle.jpg')
 
-dest_and = cv2.bitwise_and(image2, image1, mask=None)
-
+# operacion AND
+""" dest_and = cv2.bitwise_and(image2, image1, mask=None)
 cv2.imshow('Bitwise And', dest_and)
+if cv2.waitKey(0) & 0xff == 27:
+    cv2.destroyAllWindows() """
 
+# operacion OR
+dest_or = cv2.bitwise_or(image2, image1, mask=None)
+cv2.imshow('Bitwise OR', dest_or)
 if cv2.waitKey(0) & 0xff == 27:
     cv2.destroyAllWindows()
