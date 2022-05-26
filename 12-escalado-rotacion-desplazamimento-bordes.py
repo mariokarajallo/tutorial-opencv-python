25f5969be8b6a26bd2731128c80b8f4a3184f0c1
mariokarajallo <mario_karajallo@hotmail.com>
2022-05-26 15:57:58 -0400

    agregando archivo 12 el  metodo ejemplo de  rotacion  de imagen

--- 12-escalado-rotacion-desplazamimento-bordes.py
+++ 12-escalado-rotacion-desplazamimento-bordes.py
@@ -4,11 +4,23 @@ import numpy as np
 
 FILE_NAME = 'image\cat.jpg'
 
-#escalado de imagen
-try:
+# escalado de imagen
+# try:
+#     img = cv2.imread(FILE_NAME)
+#     (height, width) = img.shape[:2]
+#     res = cv2.resize(img, (int(width / 2), int(height / 2)),
+#                      interpolation=cv2.INTER_CUBIC)
+#     cv2.imwrite('resultado-escala.jpg', res)
+# except IOError:
+#     print('Error while reading files !!!')
+
+# Rotación de una imagen
+try: 
     img = cv2.imread(FILE_NAME)
-    (height, width) = img.shape[:2]
-    res = cv2.resize(img, (int(width / 2), int(height / 2)),interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite('resultado escala.jpg', res)
-except IOError:
-    print('Error while reading files !!!')
+    (rows, cols) = img.shape[:2] 
+    M = cv2.getRotationMatrix2D((cols / 2, rows / 2), 45, 1) 
+    res = cv2.warpAffine(img, M,(cols, rows)) 
+    cv2.imwrite('resultado-rotacion.jpg', res) 
+except IOError: 
+    print ('Error while reading files !!!') 
+
