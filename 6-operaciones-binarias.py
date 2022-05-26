bf82caef78743491159b9c84cd7b24cedfde506e
mariokarajallo <mario_karajallo@hotmail.com>
2022-05-26 10:31:22 -0400

    agregamos operacion bit a bit NOT

--- 6-operaciones-binarias.py
+++ 6-operaciones-binarias.py
@@ -22,9 +22,17 @@ image2 = cv2.imread('image\circle.jpg')
 #     cv2.destroyAllWindows()
 
 # Operacion XOR bit a bit
-dest_xor = cv2.bitwise_xor(image1, image2, mask=None)
-cv2.imshow('Bitwise XOR', dest_xor)
+# dest_xor = cv2.bitwise_xor(image1, image2, mask=None)
+# cv2.imshow('Bitwise XOR', dest_xor)
+# if cv2.waitKey(0) & 0xff == 27:
+#     cv2.destroyAllWindows()
+
+# Operacion NOT bit a bit
+dest_not1 = cv2.bitwise_not(image1, mask=None)
+dest_not2 = cv2.bitwise_not(image2, mask=None)
+
+cv2.imshow('Bitwise NOT on image 1', dest_not1)
+cv2.imshow('Bitwise NOT on image 2', dest_not2)
+
 if cv2.waitKey(0) & 0xff == 27:
     cv2.destroyAllWindows()
-
-
