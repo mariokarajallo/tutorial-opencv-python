8acdd496537a6b9c6829bc21a4a8225fa7f60c63
mariokarajallo <mario_karajallo@hotmail.com>
2022-05-26 12:59:50 -0400

    agregando segundo ejemplo al  archivo erode practicando erocion de imagen

--- 8-erode.py
+++ 8-erode.py
@@ -4,7 +4,15 @@ import numpy as np
 path = r'image/perro.jpg'
 image = cv2.imread(path)
 window_name = 'Image'
-kernel = np.ones((5,5), np.uint8)
-image = cv2.erode(image, kernel)
+
+#ejemplo 1
+# kernel = np.ones((5, 5), np.uint8)
+# image = cv2.erode(image, kernel)
+# cv2.imshow(window_name, image)
+# cv2.waitKey(0)
+
+#ejemplo 2
+kernel = np.ones((5, 5), np.uint8)
+image = cv2.erode(image, kernel, cv2.BORDER_REFLECT)
 cv2.imshow(window_name, image)
 cv2.waitKey(0)
