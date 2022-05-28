b1ef43e6feee896240a1bb8795979200bacd1738
mariokarajallo <mario_karajallo@hotmail.com>
2022-05-28 16:29:38 -0400

    agregando el archivo python 17, adaptive-thresholding ()  umbral adaptable

--- 17-adaptive-thresholding.py
+++ 17-adaptive-thresholding.py
@@ -7,7 +7,7 @@ import numpy as np
 
 # path to input image is specified and
 # image is loaded with imread command
-image1 = cv2.imread('input1.jpg')
+image1 = cv2.imread('image/cat.jpg')
 
 # cv2.cvtColor is applied over the
 # image input with applied parameters
@@ -16,7 +16,7 @@ img = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
 
 # applying different thresholding
 # techniques on the input image
-thresh1 = cv2.adaptiveThreshold(0img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 199, 5)
+thresh1 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 199, 5)
 
 thresh2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 199, 5)
 
